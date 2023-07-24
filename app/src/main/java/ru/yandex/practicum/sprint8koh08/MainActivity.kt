c46edce8043c29b3d85136ef1e49aab03a616bb6
Aleksei Anisimov <suxelshock@gmail.com>
2023-07-24 20:49:03 +0300

    Periodic updates

--- app/src/main/java/ru/yandex/practicum/sprint8koh08/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh08/MainActivity.kt
@@ -2,10 +2,64 @@ package ru.yandex.practicum.sprint8koh08
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SPRINT_8"
+    }
+
+    lateinit var timer: Timer
+
+    lateinit var counterText: TextView
+
+    var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "$this onCreate")
+
+        counterText = findViewById(R.id.counter)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "$this onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "$this onResume")
+        timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                counter++
+                counterText.post {
+                    counterText.text = counter.toString()
+                }
+            }
+        }, 0, 1000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "$this onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "$this onStop")
+        timer.cancel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "$this onDestroy")
     }
 }
