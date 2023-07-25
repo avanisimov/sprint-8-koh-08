3b54dcd2981756d660be56bddb7ec452743f1641
Aleksei Anisimov <suxelshock@gmail.com>
2023-07-25 13:15:57 +0300

    Helpful intents

--- app/src/main/java/ru/yandex/practicum/sprint8koh08/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh08/MainActivity.kt
@@ -1,75 +1,107 @@
 package ru.yandex.practicum.sprint8koh08
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.LabeledIntent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import java.util.Timer
-import java.util.TimerTask
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ru.yandex.practicum.sprint8koh08.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        const val TAG = "SPRINT_8"
-    }
+    private lateinit var binding: ActivityMainBinding
 
-    lateinit var timer: Timer
-
-    lateinit var counterText: TextView
-
-    var counter: Int = 0
-
-    var lastOnStopTimestamp = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(TAG, "$this onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        counterText = findViewById(R.id.counter)
-
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "$this onStart time=${System.currentTimeMillis()}")
-        val onStartTimestamp = System.currentTimeMillis()
-        val delta = onStartTimestamp - lastOnStopTimestamp
-        if (delta > 3000){
-            Log.d(TAG, "Time for PIN code")
+        binding.appSettings.setOnClickListener {
+            startActivity(
+                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+            )
         }
-    }
+        binding.relaunch.setOnClickListener {
+            finish()
+            startActivity(
+                packageManager.getLaunchIntentForPackage(packageName)
+            )
+        }
+        binding.chooser.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_VIEW
+            }
+            startActivity(Intent.createChooser(sendIntent, "Объяснение зачем это нужно"))
+        }
+        binding.notification.setOnClickListener {
+            val intent = Intent()
+            intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "$this onResume")
-        timer = Timer()
-        timer.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                counter++
-                counterText.post {
-                    counterText.text = counter.toString()
+
+            //for Android 5-7
+            //intent.putExtra("app_package", packageName)
+            //intent.putExtra("app_uid", applicationInfo.uid)
+
+            // for Android 8 and above
+            intent.putExtra("android.provider.extra.APP_PACKAGE", packageName)
+
+            startActivity(intent)
+        }
+
+        binding.skype.setOnClickListener {
+            val skypeIntent = packageManager.getLaunchIntentForPackage("com.skype.raider")
+            if (skypeIntent != null) {
+                startActivity(skypeIntent)
+            } else {
+                try {
+                    startActivity(Intent(Intent.ACTION_SEND))
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "ActivityNotFoundException",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
-        }, 0, 1000)
-    }
+        }
+        binding.filter.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+            }
+            val resInfoList = packageManager.queryIntentActivities(shareIntent, 0)
+            val extraIntents = resInfoList
+                .filter {
+                    it.activityInfo.packageName.startsWith("com.google.android.apps")
+                }
+                .map { resolveInfo ->
+                    val intent = Intent()
+                    intent.component = ComponentName(
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.activityInfo.name
+                    )
+                    intent.action = Intent.ACTION_SEND
+                    intent.type = "text/plain"
+                    intent.`package` = resolveInfo.activityInfo.packageName
+                    LabeledIntent(
+                        intent,
+                        resolveInfo.activityInfo.packageName,
+                        resolveInfo.labelRes,
+                        resolveInfo.icon
+                    )
+                }.toMutableList()
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "$this onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "$this onStop time=${System.currentTimeMillis()}")
-        timer.cancel()
-        lastOnStopTimestamp = System.currentTimeMillis()
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "$this onDestroy")
+            val chooser = Intent.createChooser(extraIntents.removeAt(0), "My share")
+            chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents.toTypedArray())
+            startActivity(chooser)
+        }
     }
 }
